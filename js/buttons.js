e7b22ba2a1267c8cd968bab6212dd6ac6345213f
rjhj <rjhjuu@gmail.com>
2015-06-19 09:32:19 +0300

    More logs

--- js/buttons.js
+++ js/buttons.js
@@ -1,14 +1,42 @@
 
 $("button[name='start']").click(function(){
+
+	//getting need variables
 	var numberOfLists = parseInt($("#range1").text());
 	var numberOfElements = parseInt($("#range2").text());
 	var smallestElement = parseInt($("#range3").text());
 	var largestElement = parseInt($("#range4").text());
 
-	lists = createLists(numberOfLists,numberOfElements, smallestElement, largestElement);
+	// create lists that need to be sorted
+	var lists = createLists(numberOfLists,numberOfElements, smallestElement, largestElement);
 
+	var sortedBubble = [];
+	var sortedSmallest = [];
+
+	// prints lists that need to be sorted
 	$("p[class='log']").html("<p>Created lists:</p>");
 	for(var i = 0; i<lists.length; i++){
 		$("p[class='log']").append("<p> List " + (i+1) + ": " + lists[i] +"</p>");
 	}
+
+	// sort lists with bubbleSort
+	for(var i = 0; i<lists.length; i++){
+			sortedBubble.push(bubbleSort(lists[i]));
+		}
+
+	// sort lists with sortByFindingSmallest
+	for(var i = 0; i<lists.length; i++){
+			sortedSmallest.push(sortByFindingSmallest(lists[i]));
+		}
+	
+	$("p[class='log']").append("<br><p> Lists sorted by BubbleSort:</p>");
+	for(var i = 0; i<lists.length; i++){
+		$("p[class='log']").append("<p> List " + (i+1) + ": " + sortedBubble[i] +"</p>");
+	}
+
+	$("p[class='log']").append("<br><p> Lists sorted by FindSmallest:</p>");
+	for(var i = 0; i<lists.length; i++){
+		$("p[class='log']").append("<p> List " + (i+1) + ": " + sortedSmallest[i] +"</p>");
+	}
+
 });
